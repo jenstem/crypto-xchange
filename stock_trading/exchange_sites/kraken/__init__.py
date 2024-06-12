fd91e9b78bf92fa847740ec0a893597f40c7064a
jenstem <jennifer.stemkowski@gmail.com>
2024-06-12 11:24:21 -0400

    Add get_signature func to generate signature for API requests

--- stock_trading/exchange_sites/kraken/__init__.py
+++ stock_trading/exchange_sites/kraken/__init__.py
@@ -1,5 +1,10 @@
 import os
+import base64
 from dotenv import load_dotenv
+import hashlib
+import hmac
+import urllib
+
 
 load_dotenv()
 
@@ -9,3 +14,13 @@ assetAttributeNames = {
     'LTC': 'Litecoin',
     'USDT': 'Tether',
 }
+
+# Generate a signature for the API requests
+def get_signature(urlpath, data):
+    postdata = urllib.parse.urlencode(data)
+    encoded_message = (str(data['nonce']) + postdata).encode()
+    message = urlpath.encode() + hashlib.sha256(encoded_message).digest()
+
+    signature = hmac.new(base64.b64decode(os.environ.get("KRAKEN_API_PRIVATE")), message, hashlib.sha512)
+    sigdigest = base64.b64encode(signature.digest())
+    return sigdigest.decode()
