40284ebbfdc3a50fe993141e86abd4ae186e6499
jenstem <jennifer.stemkowski@gmail.com>
2024-06-12 11:13:19 -0400

    Add assetAttributeNames to abbr currencies

---
+++ stock_trading/exchange_sites/kraken/__init__.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+assetAttributeNames = {
+    'BTC': 'Bitcoin',
+    'ETH': 'Ethereum',
+    'LTC': 'Litecoin',
+    'USDT': 'Tether',
+}
