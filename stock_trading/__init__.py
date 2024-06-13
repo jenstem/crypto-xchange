ab84aded0f01ec25c2c7cb3513d1efce5bfc2bf4
jenstem <jennifer.stemkowski@gmail.com>
2024-06-13 14:22:46 -0400

    Add func to notify when market is closed market_closed

--- stock_trading/__init__.py
+++ stock_trading/__init__.py
@@ -1,9 +1,15 @@
 import os
 import json
 
+
 def stock_watcher(data):
     print("------------")
     print(json.loads(data))
     print("------------")
 
+
 # Notification for closing
+def market_closed():
+    print("------------")
+    print("Stock market is closed")
+    print("------------")
