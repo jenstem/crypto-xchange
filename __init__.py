a420fdfc7ab9757382f5a4ef9d8c8a093931dd11
jenstem <jennifer.stemkowski@gmail.com>
2024-06-17 15:50:17 -0400

    Add build_trading_pairs func to check if pairs are valid + store valid pairs

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
 import json
+import pickle
 from stock_trading.exchange_sites.kraken import *
 
 load_dotenv()
+
 
 client = os.environ.get("KRAKEN_API_KEY")
 
@@ -19,10 +21,32 @@ prices = client.get_all_tickers()
 
 address = client.get_deposit_address(coin='BTC')
 
+
 def stock_watcher(ws, data):
     print("----------")
     print(json.loads(data))
     print("----------")
 
+
 def market_close():
     print("Market is closed")
+
+
+def build_trading_pairs(*args) -> list:
+    pairs = []
+    stored_pairs_path = "".join(args)
+    if os.path.exists(stored_pairs_path):
+        with open(stored_pairs_path, 'rb') as f:
+            pairs = pickle.load(f.read())
+    else:
+        for p in args:
+            i = 0
+            while args.__len__() > i:
+                pair = p + args[i]
+                if pair.upper() in get_trading_pairs():
+                    pairs.append(pair.upper())
+                i += 1
+            with open(stored_pairs_path, 'wb') as f:
+                f.write(pickle.dumps(pairs))
+
+    return pairs
