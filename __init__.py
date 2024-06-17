28fe549102e05c9b88c138b4f7597f2e9251bad2
jenstem <jennifer.stemkowski@gmail.com>
2024-06-17 15:03:41 -0400

    Fetch exchange info from API + append stock symbol to pairs list

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+import json
+from stock_trading.exchange_sites.kraken import get_deposit_address
+
+load_dotenv()
+
+client = os.environ.get("KRAKEN_API_KEY")
+
+def get_trading_pairs() -> list:
+    exchange_info = client.get_exchange_info()
+    trading_pairs = []
+    for s in exchange_info['symbols']:
+        trading_pairs.append(s['symbol'])
+
+    return trading_pairs
+
+prices = client.get_all_tickers()
+
+address = client.get_deposit_address(coin='BTC')
