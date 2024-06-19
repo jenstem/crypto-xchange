0b6aacd781b5be28d8586e65cfa8d1d9aa9198f7
jenstem <jennifer.stemkowski@gmail.com>
2024-06-19 11:47:47 -0400

    Func exchange_watcher receives data from API + prints data

--- stock_trading/__init__.py
+++ stock_trading/__init__.py
@@ -1,3 +1,4 @@
+import json
 from models import Database
 from stock_trading.exchange_sites.kraken.wsbook import WSBookKraken
 
@@ -6,3 +7,14 @@ class CryptoXchanges(WSBookKraken):
     def __init__(self, args):
         WSBookKraken.__init__(self, args)
         self.database = Database()
+
+    def exchange_watcher(self, data):
+        try:
+            prices = json.loads(data)
+            k_prices = list(self.get_data())[0]
+            print("Kraken", k_prices)
+            t_text = "Buy {:.5f} on Kraken".format(float(k_prices[0]))
+            print(t_text)
+
+        except Exception as e:
+            print(str(e))
